2cb074de8c511ce7c1a55aa80e191a3db8edd9bf
muhammad tayyab <147498219+tayyab129@users.noreply.github.com>
2024-02-15 20:40:47 +0500

    Carusel

--- src/components/Home/MedicalServicesCards.jsx
+++ src/components/Home/MedicalServicesCards.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import carousel styles
 import Gynocologyicon from "../../icons/gynecology.png";
 import HormonalExamination from "../../icons/hormonal-examination.png";
 import InternalMedicine from "../../icons/Internal-medicine.png";
 import Ionm from "../../icons/ionm.png";
 import Anesthesiology from "../../icons/anesthesiology.png";
+import FamilyMedicine from "../../icons/family-medicine.png";
+import PainManagement from "../../icons/pain-management.png";
+import PhysicalTherapy from "../../icons/physical-therapy.png";
+import PlasticSurgery from "../../icons/plastic-surgery.png";
+import EmergencyMedicine from "../../icons/emergency-medicine.png";
+import Padiatry from "../../icons/Podiatry.png";
+import Deramatology from "../../icons/neurology.png";
+import Ophthalmology from "../../icons/ophthalmology.png";
+import Neurosurgery from "../../icons/neurosurgery.png";
+import Orthopedics from "../../icons/orthopedics.png";
+import Pathology from "../../icons/pathology.png";
+import Pediatrics from "../../icons/pediatrics.png";
 
 const serviceSpecialties = [
   {
@@ -26,27 +40,56 @@ const serviceSpecialties = [
     title: "Anesthesiology",
     image: Anesthesiology,
   },
-  // {
-  //   title: "Psychiatry",
-  //   image: Psychiatry,
-  // },
-  // {
-  //   title: "Radiology",
-  //   image: Radiology,
-  // },
-  // {
-  //   title: "Urology",
-  //   image: Urology,
-  // },
-  // {
-  //   title: "Neurology",
-  //   image: Neurology,
-  // },
-  // {
-  //   title: "Pain Management",
-  //   image: Painmanagement,
-  // },
+  {
+    title: "Family Medicine",
+    image: FamilyMedicine,
+  },
+  {
+    title: "Pain Management",
+    image: PainManagement,
+  },
+  {
+    title: "Physical Therapy",
+    image: PhysicalTherapy,
+  },
+  {
+    title: "Plastic Surgery",
+    image: PlasticSurgery,
+  },
+  {
+    title: "Emergency Medicine",
+    image: EmergencyMedicine,
+  },
+  {
+    title: "Padiatry",
+    image: Padiatry,
+  },
+  {
+    title: "Deramatology",
+    image: Deramatology,
+  },
+  {
+    title: "Ophthalmology",
+    image: Ophthalmology,
+  },
+  {
+    title: "Neurosurgery",
+    image: Neurosurgery,
+  },
+  {
+    title: "Orthopedics",
+    image: Orthopedics,
+  },
+  {
+    title: "Pathology",
+    image: Pathology,
+  },
+  {
+    title: "Pediatrics",
+    image: Pediatrics,
+  },
 ];
+
 const MedicalServicesCards = () => (
   <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4 mt-10 mx-5 md:mx-40">
     {serviceSpecialties.map((specialty, index) => (
@@ -68,5 +111,35 @@ const MedicalServicesCards = () => (
     ))}
   </div>
 );
+// const MedicalServicesCards = () => (
+//   <div className="mt-10 mx-10 md:mx-40">
+//     <Carousel
+//       showThumbs={false}
+//       dynamicHeight={false}
+//       autoPlay={true} // Set autoPlay to true for automatic slideshow
+//       interval={2000} // Specify the interval between slides (in milliseconds)
+//       showStatus={false} // Hide the status indicators (dots)
+//       className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4"
+//     >
+//       {serviceSpecialties.map((specialty, index) => (
+//         <div
+//           key={index}
+//           className="col-span-1 p-6 text-center bg-white rounded-lg shadow-md"
+//         >
+//           <div className="flex flex-col items-center justify-center">
+//             <img
+//               className="w-[150px] h-[120px]"
+//               src={specialty.image}
+//               alt={specialty.title}
+//             />
+//             <h3 className="font-bold text-elementcolor mt-3">
+//               {specialty.title}
+//             </h3>
+//           </div>
+//         </div>
+//       ))}
+//     </Carousel>
+//   </div>
+// );
 
 export default MedicalServicesCards;
